6732d6cc9b0b0f261b1566cae8508efe58640cbc
patrickhallen1992 <patrickh.allen1992@gmail.com>
2024-06-05 21:06:37 -0400

    add working Hello World! code

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 def hello_world():
     # Remove the # in the line below and delete the word pass
-    # print("Hello World!")
-
-    pass
+    print("Hello World!")
+    
    
 
 # Do not modify the code below this line
